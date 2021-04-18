d7ee19aee8586652707a6b048c42b545be302a83
Maksym Hodunov <m.godunov@yandex.ru>
2021-04-18 16:25:53 +0300

    feat: adds feature that allows save contacts in local storage

--- src/App.js
+++ src/App.js
@@ -7,14 +7,21 @@ import './App.css';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(savedContacts);
+    parsedContacts && this.setState({ contacts: [...parsedContacts] });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   submitHandler = newContact => {
     const { contacts } = this.state;
